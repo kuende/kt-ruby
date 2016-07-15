90d1869bc830638f3f2adc9fd62ca330d2f6e716
Teodor <teodor.pripoae@gmail.com>
2016-07-15 16:17:47 +0300

    Allow focus in specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,5 +8,6 @@ HOST = "127.0.0.1"
 PORT = 1978
 
 RSpec.configure do |config|
-  # some (optional) config here
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
 end
